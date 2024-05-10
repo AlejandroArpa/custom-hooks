e75dbea437571fafff67d3e8e06337b02dfe5d2f
Alejandro Ramirez <alejorp195@hotmail-com>
2024-05-10 06:18:55 -0500

    Primer commit

---
+++ useTodo.js
@@ -0,0 +1,41 @@
+import { useEffect, useReducer } from 'react';
+import { todoReducer } from '../useReducer/todoReducer';
+
+const initialState = JSON.parse (localStorage.getItem( 'todos')) || [];
+
+export const useTodo = () => {
+
+    const [ todos, dispatch ] = useReducer( todoReducer, initialState );
+
+	useEffect(() => {
+		localStorage.setItem( 'todos', JSON.stringify(todos) )
+	}, [todos])
+
+	const onNewTodo = ( todo ) =>{
+		const action = {
+			type: 'Add Todo',
+			payload: todo
+		}
+		dispatch( action );
+	}
+
+	const onRmClic = (todo) => {
+		const action = {
+			type: 'Remove Todo',
+			payload: todo
+		}
+		dispatch( action );
+	}
+
+	const onToggleTodo = ( todo ) => {
+		const action = {
+			type: 'Toggle Todo',
+			payload: todo
+		}
+		dispatch( action );
+	}
+
+    return (
+    {todos, onNewTodo, onRmClic, onToggleTodo  }
+    )
+}
